06ef67b59107678b69032a291a0726718bccd4e1
Cédric Sauriol <cedric.sauriol@hotmail.com>
2019-06-13 15:46:58 -0400

    Added tests for day01 part one.

--- src/day01/day01.c
+++ src/day01/day01.c
@@ -1,7 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
 #include "day01.h"
 
-char* day01_part_one() {
-    return "one";
+char* day01_part_one(char* input) {
+    int length = strlen(input);
+    int counter = 0;
+
+    for(int i = 0; i < length; i++) {
+        counter += '(' == input[i] ? 1 : -1;
+    }
+
+    char* buffer = malloc(10);
+    sprintf(buffer, "%d", counter);
+
+    return buffer;
 }
 
 char* day01_part_two() {
--- test/day01/day01test.c
+++ test/day01/day01test.c
@@ -5,8 +5,32 @@
 #include "day01test.h"
 
 void test_day01_part_one() {
-    char* result = day01_part_one();
-    assert(0 == strcmp("one", result));
+    char* result = day01_part_one("(())");
+    assert(0 == strcmp("0", result));
+
+    result = day01_part_one("()()");
+    assert(0 == strcmp("0", result));
+
+    result = day01_part_one("(((");
+    assert(0 == strcmp("3", result));
+
+    result = day01_part_one("(()(()(");
+    assert(0 == strcmp("3", result));
+
+    result = day01_part_one("))(((((");
+    assert(0 == strcmp("3", result));
+
+    result = day01_part_one("())");
+    assert(0 == strcmp("-1", result));
+
+    result = day01_part_one("))(");
+    assert(0 == strcmp("-1", result));
+
+    result = day01_part_one(")))");
+    assert(0 == strcmp("-3", result));
+
+    result = day01_part_one(")())())");
+    assert(0 == strcmp("-3", result));
 }
 
 void test_day01_part_two() {
